adc6ca94f4dfeae291766dda421298bddff768c9
wsulais-kfupm <s202032440@kfupm.edu.sa>
2022-12-14 21:16:44 +0300

    feat: Add search methods for `Package` repository

--- src/main/java/edu/kfupm/ics321/repo/PackageRepository.java
+++ src/main/java/edu/kfupm/ics321/repo/PackageRepository.java
@@ -1,7 +1,19 @@
 package edu.kfupm.ics321.repo;
 
-
 import edu.kfupm.ics321.model.Package;
+import edu.kfupm.ics321.model.PackageType;
+import edu.kfupm.ics321.model.PackageStatus;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PackageRepository extends PagingAndSortingRepository<Package, Long> {}
+public interface PackageRepository extends PagingAndSortingRepository<Package, Long> {
+    List<Package> findByType(@Param("type") PackageType type);
+
+    List<Package> findByStatus(@Param("status") PackageStatus status);
+
+    List<Package> findByFinalDeliveryDate(@Param("date") Date finalDeliveryDate);
+}
