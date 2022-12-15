a3c47e99d5fd8617b8db3a702050ec6be2e5fd32
wsulais-kfupm <s202032440@kfupm.edu.sa>
2022-12-15 19:11:47 +0300

    feat: Implement `findBetween` report

--- src/main/java/edu/kfupm/ics321/repo/PackageRepository.java
+++ src/main/java/edu/kfupm/ics321/repo/PackageRepository.java
@@ -4,9 +4,14 @@ import edu.kfupm.ics321.model.Package;
 import edu.kfupm.ics321.model.PackageType;
 import edu.kfupm.ics321.model.PackageStatus;
 import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -15,5 +20,11 @@ public interface PackageRepository extends PagingAndSortingRepository<Package, L
 
     List<Package> findByStatus(@Param("status") PackageStatus status);
 
-    List<Package> findByFinalDeliveryDate(@Param("date") Date finalDeliveryDate);
+    List<Package> findByFinalDeliveryDate(@Param("date") LocalDate finalDeliveryDate);
+
+    @Query(nativeQuery = true, value="select * from Package where FINAL_DELIVERY_DATE between :from and :to AND (TYPE = :#{#type?.ordinal()} OR :type IS NULL) AND (STATUS = :#{#status?.ordinal()} OR :status IS NULL)")
+    List<Package> findBetween(@Param("from") @DateTimeFormat(iso = ISO.DATE) LocalDate from,
+                              @Param("to") @DateTimeFormat(iso = ISO.DATE) LocalDate to,
+                              @Param("type") Optional<PackageType> type,
+                              @Param("status") Optional<PackageStatus> status);
 }
